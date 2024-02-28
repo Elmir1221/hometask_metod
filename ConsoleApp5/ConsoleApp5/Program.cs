db4ab2aa139282a028996bafbe4e7f3cca5c4cb7
Elmir1221 <158136055+Elmir1221@users.noreply.github.com>
2024-02-28 14:11:55 +0400

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp5;
+
+User user = new User("sddsa","sadd",12);
+user.SetPassword("assd");
+Console.WriteLine(user.GetPassword());
---
+++ ConsoleApp5/ConsoleApp5/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class User
+    {
+        public string name;
+        public string surName;
+        public int age;
+        private string password;
+
+        public User (string name, string surName, int age) 
+        {
+            this.age = age;
+            this.surName = surName;
+            this.name = name;
+        }
+    
+         public string GetPassword()
+         {
+            return password;
+         }
+         public void SetPassword(string password)
+         {
+            this.password = password;   
+        
+         }
+      
+
+    }
+    
+   
+}
